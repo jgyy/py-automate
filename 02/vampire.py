90c4edc602ce6b2c58e853821fd2c2e4564d90fc
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2021-10-10 23:04:29 +0800

    vampire

---
+++ 02/vampire.py
@@ -0,0 +1,21 @@
+"""
+Vampire
+"""
+
+
+def wrapper(name, age):
+    """
+    wrapper function
+    """
+    if name == 'Alice':
+        print('Hi, Alice.')
+    elif age < 12:
+        print('You are not Alice, kiddo.')
+    elif age > 2000:
+        print('Unlike you, Alice is not an undead, immortal vampire.')
+    elif age > 100:
+        print('You are not Alice, grannie.')
+
+
+if __name__ == "__main__":
+    wrapper("a", 1)
